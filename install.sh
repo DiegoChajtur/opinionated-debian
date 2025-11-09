95152804294c4b401f01944c69221ad95f03d55b
DiegoChajtur <diego.chajtur@gmail.com>
2025-11-08 19:58:27 -0500

    chg: configure ssh

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 set -e
 
 # Configuration
-REPO_USER="DiegoChajtur"
-REPO_NAME="opinionated-debian"
-REPO_BRANCH="main"
+read -p "Enter your a comment (can be your email to easy remember) to configure your sshkey configuration:" SSH
+
+ssh-keygen -t rsa -b 4096 -C "$SSH" 
+
 
 # Ensure non-interactive mode
 export DEBIAN_FRONTEND=noninteractive
@@ -30,7 +31,8 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] http
 
 # Install packages
 sudo apt-get update
-sudo apt-get install -y curl wget git zsh tmux fzf ripgrep fd-find bat htop tree jq fonts-firacode ffmpeg imagemagick firefox-esr vlc libreoffice krita python3 nodejs npm golang default-jdk earlyoom code
+sudo apt-get install -y curl wget git zsh tmux fzf ripgrep fd-find bat htop tree jq fonts-firacode ffmpeg firefox-esr vlc libreoffice python3 nodejs npm golang default-jdk earlyoom code gnome-themes-extra gtk2-engines-murrine gimp 
+
 
 # Install pnpm globally
 sudo npm install -g pnpm
@@ -46,55 +48,26 @@ sudo chsh -s /usr/bin/zsh $USER
 # Install Oh My Zsh
 export RUNZSH=no
 export CHSH=no
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# Install zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# Configure .zshrc with Powerlevel10k
-cat > ~/.zshrc <<'ZSHRC'
-# Enable Powerlevel10k instant prompt
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
+cd /tmp
 
-export ZSH="$HOME/.oh-my-zsh"
-ZSH_THEME="powerlevel10k/powerlevel10k"
+wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb
 
-DISABLE_AUTO_UPDATE="true"
-DISABLE_MAGIC_FUNCTIONS="true"
+sudo apt install ./protonvpn-stable-release_1.0.8_all.deb
 
-plugins=(git docker sudo zsh-autosuggestions)
-skip_global_compinit=1
+sudo apt update
 
-source $ZSH/oh-my-zsh.sh
+sudo apt install proton-vpn-gnome-desktop
 
-# Aliases
-alias ll='ls -lah'
-alias gs='git status'
-alias ga='git add'
-alias gc='git commit'
-alias gp='git push'
 
-HISTSIZE=10000
-SAVEHIST=10000
+wget https://proton.me/download/PassDesktop/linux/x64/ProtonPass.deb
 
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-ZSHRC
+sudo apt install ./ProtonPass.deb
 
-# Create Powerlevel10k config
-cat > ~/.p10k.zsh <<'P10K'
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
 
-typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
-typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)
-typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status command_execution_time time)
-typeset -g POWERLEVEL9K_DIR_FOREGROUND=blue
-typeset -g POWERLEVEL9K_VCS_FOREGROUND=green
-P10K
 
